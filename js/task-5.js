1f959ff45dfdaf7d87f5d25559758b92eb9e485e
Dariia K <da23v12@gmail.com>
2025-03-14 12:20:25 +0000

    3 last tasks

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const button = document.querySelector(".change-color");
+const textColor = document.querySelector(".color");
+
+const handleClick = () => {
+const randomColor = getRandomHexColor();
+document.body.style.backgroundColor = randomColor;
+  textColor.textContent = randomColor;
+};
+
+button.addEventListener("click", handleClick);
